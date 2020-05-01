044c2073f78a02a6e488e688531f5e662891e25c
cameron <cameronshaw95@gmail.com>
2020-05-01 18:40:01 +1200

    test reload for server

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('test cdcd test test plz work!'))
+app.get('/', (req, res) => res.send('test to see if my server is reloading changes!'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
